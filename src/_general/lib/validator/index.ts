27aa094021cf61092128317fe292cfaae753618f
inatatsu-tatsuhiro <tatsuhiro.9699@gmail.com>
2022-01-27 23:16:59 +0900

    :+1: validator

---
+++ src/_general/lib/validator/index.ts
@@ -0,0 +1,27 @@
+import { RefObject } from 'react'
+
+const ADDRESS_PATTERN = /[A-Z0-9]{39}/
+const PRIKEY_PATTERN = /[A-F0-9]{64}/
+
+export const validateRef = (
+  ref: RefObject<HTMLInputElement>,
+  f?: (s: string) => string
+): string => {
+  if (ref === null || ref.current === null) return ''
+  if (f === undefined) return ref.current.value
+  return f(ref.current.value)
+}
+
+export const validateAddress = (address: string): string => {
+  if (address.length === 39 && ADDRESS_PATTERN.test(address)) {
+    return address
+  }
+  return ''
+}
+
+export const validatePrivateKey = (priKey: string): string => {
+  if (priKey.length === 64 && PRIKEY_PATTERN.test(priKey)) {
+    return priKey
+  }
+  return ''
+}
