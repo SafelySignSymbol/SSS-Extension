8f687d3fa86de298f459e2b5c8a267e6dd6d61f3
inatatsu-tatsuhiro <tatsuhiro.9699@gmail.com>
2022-01-27 18:29:26 +0900

    :+1: inject script

--- src/content_scripts/index.ts
+++ src/content_scripts/index.ts
@@ -1,1 +1,10 @@
 export {};
+const injectScript = function (file: string, node: any) {
+  const th = document.getElementsByTagName(node)[0];
+  const s = document.createElement("script");
+  s.setAttribute("type", "text/javascript");
+  s.setAttribute("src", chrome.runtime.getURL(file));
+  return th.appendChild(s);
+};
+
+injectScript("inject_script.js", "body");
