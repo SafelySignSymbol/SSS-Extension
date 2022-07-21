1709ac97cb4f4ca09b957a8958a52914ef976e25
inatatsu-tatsuhiro <tatsuhiro.9699@gmail.com>
2022-07-21 18:14:33 +0900

    :+1: MAX FEEを表示

--- src/Popup/pages/components/TransactionInfo.tsx
+++ src/Popup/pages/components/TransactionInfo.tsx
@@ -8,9 +8,7 @@ import { TransactionMapping } from 'symbol-sdk'
 import { TransactionURI } from 'symbol-uri-scheme'
 import Typography from '../../../_general/components/Typography'
 import { getTransactionType } from '../../../_general/lib/TransactionType'
-// import { IconButton } from '@mui/material'
-// import { IconContext } from 'react-icons'
-// import { MdUpdate } from 'react-icons/md'
+import { Chip, Tooltip } from '@mui/material'
 
 export type Props = {
   transaction: string
@@ -27,13 +25,11 @@ const Component: React.VFC<Props> = ({ transaction }) => {
       <Header>
         <TxType>
           <Typography text={getTransactionType(tx.type)} variant="h5" />
+          <Tooltip title="MAX FEE">
+            <Chip label={`${Number(tx.maxFee.toString()) / 1000000} XYM`} />
+          </Tooltip>
         </TxType>
         <Box sx={{ flexGrow: 1 }} />
-        {/* <IconButton onClick={() => console.log('click')}>
-          <IconContext.Provider value={{ size: '24px' }}>
-            <MdUpdate />
-          </IconContext.Provider>
-        </IconButton> */}
       </Header>
       <Content>
         <TransactionCard transaction={tx} />
@@ -57,6 +53,7 @@ const Content = styled('div')({
 })
 
 const TxType = styled('div')({
-  display: 'grid',
-  alignItems: 'center',
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'space-between',
 })
